00d0dd6318a5d01758f6a99901bd28cec9a85dfd
purofle <purofle@gmail.com>
2023-05-02 21:34:14 +0800

    refactor(frontend): modify page ui

--- frontend/src/components/input.tsx
+++ frontend/src/components/input.tsx
@@ -5,10 +5,26 @@ interface InputProps {
   name: string
   type: string
   autoComplete: string
-  required: boolean
+  required: boolean,
+  onEvent?: {(event: React.ChangeEvent<HTMLInputElement>): void}
+  children?: JSX.Element
 }
 
 export default function NormalInput(props: InputProps) {
-  return <input id={props.id} name={props.name} type={props.type} autoComplete={props.autoComplete} required={props.required}
-                       className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+  return <div className="relative">
+  <input id={props.id}
+                name={props.name}
+                type={props.type}
+                autoComplete={props.autoComplete}
+                required={props.required}
+                onChange={props.onEvent}
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm
+                ring-1 ring-inset ring-gray-300 placeholder:text-gray-400
+                focus:ring-2 focus:ring-inset focus:ring-indigo-600
+                sm:text-sm sm:leading-6
+                [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none"
+  >
+    </input>
+    <div className="absolute inset-y-0 right-0 pr-3 flex items-center">{props.children}</div>
+    </div>
 }
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -4,41 +4,49 @@ import NormalInput from "@/components/input";
 
 export default function Home() {
   const [phone, setPhone] = useState("")
-
+  const [code, setCode] = useState("")
+  const disabledButton = code.length == 0
+  console.log(disabledButton)
   return (
     <>
       <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
           <Image className="mx-auto h-10 w-auto" src=""
-               alt="Your Company" width={200} height={200} />
-            <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">注册</h2>
+                 alt="Your Company" width={200} height={200}/>
+          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">注册</h2>
         </div>
 
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
           <form className="space-y-6" action="#" method="POST">
             <div>
-              <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">Email address</label>
+              <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">手机号</label>
               <div className="mt-2">
-                <NormalInput id="phone" name="phone" type="tel" autoComplete="tel" required={true} />
+                <NormalInput id="phone" name="phone" type="tel" autoComplete="tel" required={true} onEvent={(event) => {
+                  setPhone(event.target.value)
+                }}/>
               </div>
             </div>
 
             <div>
               <div className="flex items-center justify-between">
-                <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">Password</label>
-                <div className="text-sm">
-                  <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">Forgot password?</a>
-                </div>
+                <label htmlFor="number" className="block text-sm font-medium leading-6 text-gray-900">验证码</label>
               </div>
               <div className="mt-2">
-                <NormalInput id="password" name="password" type="password" autoComplete="current-password" required={true} />
+                <NormalInput id="number" name="number" type="number" autoComplete="number" required={true} onEvent={(event) => {
+                  setCode(event.target.value)
+                }}>
+                  <button type="button" className="">
+                    <label className="block text-sm font-medium leading-6 text-indigo-600">获取验证码</label>
+                  </button>
+                </NormalInput>
+
               </div>
             </div>
-
             <div>
               <button type="submit"
-                      className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign
-                in
+                      className={`flex w-full justify-center rounded-md px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 bg-indigo-600 ${disabledButton ? "text-gray-400" : "text-white"}`}
+              >
+                注册
               </button>
             </div>
           </form>
